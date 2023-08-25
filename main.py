cc177334c2fa519c7c566a1f358d2f735f77256f
JordanBarrera1 <12695572-JordanBarrera1@users.noreply.replit.com>
2023-08-25 14:36:48 +0000

    Initial Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import test
+import test2
+print("first day in python")
+print("hello world")
+name= "Jordan"
+age=15
+print("name", name, "age", age)
+print("Hello "+ name + "! you are " + str(age) + " years old.")
+#the (+) is concatenation, use when you want to insert variables in a sentence.
+city="chicago"
+name_of_street= "5236 south barrel st"
+zipcode= 40560
+phone_number= "312-546-5678"
